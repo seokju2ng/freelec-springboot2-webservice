053931dfb6335c9ac1c232ef153cf288f632a01c
seokju2ng <ehddnwnd@naver.com>
2020-06-11 21:45:28 +0900

    9.5 배포 자동화 구성

--- src/main/java/com/jojoldu/book/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .and()
                 .authorizeRequests()    // URL별 권한 관리를 설정하는 옵션의 시작점.
                                         // authorizeRequests가 선언되어야만 antMatchers 옵션을 사용할 수 있음
-                .antMatchers("/", "/css/**", "images/**", "/js/**", "/h2-console/**").permitAll()
+                .antMatchers("/", "/css/**", "images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
                 .antMatchers("/api/v1/**").hasRole(Role.USER.name())
                     // 권한 관리 대상을 지정하는 옵션
                     // URL, HTTP 메소드별로 관리가 가능
